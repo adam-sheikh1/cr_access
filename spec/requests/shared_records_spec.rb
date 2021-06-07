329188f759a982a7863c1bb3e27dea0a830deb4d
John <john.ibrahim@commitlabs.us>
2021-06-07 23:30:10 +0500

    added missing case for shared vaccination record list

--- spec/requests/shared_records_spec.rb
+++ spec/requests/shared_records_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "SharedRecords", type: :request do
   let(:user) { create(:user) }
   let(:cr_access_group) { create(:cr_access_group, :with_cr_access_data, :with_cr_group) }
+  let(:vaccination_records) { create_list(:vaccination_record, 2, :with_cr_access_data, :with_vaccination_users) }
 
   describe "GET #show" do
     context 'when signed in' do
@@ -24,6 +25,24 @@ RSpec.describe "SharedRecords", type: :request do
 
           expect(response).to have_http_status(:success)
           expect(response.body).to include("Vaccination History")
+        end
+
+        it "fetches the correct vaccination record list" do
+          other_vaccination_record = create(:vaccination_record, :with_cr_access_data, :with_vaccination_users)
+
+          sign_in user
+          cr_access_group.accepted!
+          user.all_cr_data << cr_access_group.cr_access_data
+          new_vaccination_records = cr_access_group.cr_access_data.vaccination_records << vaccination_records
+          get shared_record_path(cr_access_group)
+
+          expect(response).to have_http_status(:success)
+
+          new_vaccination_records.each do |vr|
+            expect(response.body).to include(vr.lot_number)
+          end
+
+          expect(response.body).to_not include(other_vaccination_record.lot_number)
         end
       end
     end
